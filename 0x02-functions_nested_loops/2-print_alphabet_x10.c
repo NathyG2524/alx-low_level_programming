957d625def08f7689daadd59dc9c1fb4c8f63498
NathyG2524 <Nathygirma2524@gmail.com>
2021-08-31 07:29:27 -0700

    putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    har c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+return (0);
+}
