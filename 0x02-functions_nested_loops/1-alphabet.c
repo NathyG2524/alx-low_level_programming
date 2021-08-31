74405932c6a8d6968742f159cfe0915375e8e89d
NathyG2524 <Nathygirma2524@gmail.com>
2021-08-31 09:25:26 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
  */
 void print_alphabet(void)
 {
-  char c;
+	char c;
 
-  for (c = 'a'; c <= 'z'; c++)
-    {
-      _putchar(c);
-    }
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
