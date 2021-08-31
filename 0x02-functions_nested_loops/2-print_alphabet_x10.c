2972da32f9220a2f3b98a891e76f5e04a67c943b
NathyG2524 <Nathygirma2524@gmail.com>
2021-08-31 09:36:39 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	int i;
+	for (i = 0; i<9; ++i)
+	  {
+	    for (c = 'a'; c <= 'z'; c++)
+	      {
+		_putchar(c);
+	      }
+	    -putchar('\n');
+	  }
+}
