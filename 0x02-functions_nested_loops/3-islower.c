d153b51bb2ae3a21894f012287d3c3677f6daf63
NathyG2524 <Nathygirma2524@gmail.com>
2021-08-31 10:11:26 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *shows 1 if the input is a lowercse
+ *else shows  0
+ */
+int_islower(int c)
+{
+  if (c >= 97 && c <= 122)
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+  _putchar('/n');
+}
