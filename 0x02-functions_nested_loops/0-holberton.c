3a799c4fc8b28e35209773edc4a429a16e3d30fe
NathyG2524 <Nathygirma2524@gmail.com>
2021-08-31 06:53:08 -0700

    putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 /**
-*main - prints Holberton
-*Return: 0
-*/
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	char c[] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
 	_putchar('\n');
 	return (0);
 }
