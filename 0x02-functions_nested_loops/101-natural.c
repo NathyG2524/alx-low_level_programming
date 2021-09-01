24da06be728ed24e3771df59b5080f31cfe359fd
NathyG2524 <Nathygirma2524@gmail.com>
2021-08-31 23:50:12 -0700

    natural number

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a,b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
