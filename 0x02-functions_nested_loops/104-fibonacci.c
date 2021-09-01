157ab55ab15281208143b4ef5b6170482f14d689
NathyG2524 <Nathygirma2524@gmail.com>
2021-09-01 12:40:37 -0700

    fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,30 +24,4 @@ int main(void)
                 }
         }
         return (0);
-}#include <stdio.h>
-/**
- *main - This is a list of
- *fabionacci series up to 50
- *Return: Nothing.
- */
-int main(void)
-{
-        unsigned long i, j, k, l;
-
-        i = 0;
-        j = 1;
-        for (k = 0; k < 98; k++)
-        {
-                l = i + j;
-                i = j;
-                j = l;
-                printf("%lu", l);
-                if (k == 97)
-                        putchar('\n');
-                else
-                {
-                        printf(", ");
-                }
-        }
-        return (0);
 }
