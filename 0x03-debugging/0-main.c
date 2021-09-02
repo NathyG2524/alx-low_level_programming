51ebae7a134cda6f5b646a3b5bf0fc4f9c5e97e3
NathyG2524 <Nathygirma2524@gmail.com>
2021-09-02 01:24:50 -0700

    positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,17 +5,12 @@
 * Return: 0
 */
 
-int main(void)
+int positive_or_negative(void)
 {
         int i;
 
-        i = 98;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+        i = 0;
+         positive_or_negative(i);
 
         return (0);
 }
