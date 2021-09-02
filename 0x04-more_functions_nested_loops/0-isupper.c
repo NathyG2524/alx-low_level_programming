797b117340f783d2c6a84ce5c86f371932dbb53a
NathyG2524 <Nathygirma2524@gmail.com>
2021-09-02 07:44:26 -0700

    upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - check for upper case
+ *Return: 0 or 1
+ */
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'B')
+		return (1);
+	else
+		return (0);
+}
