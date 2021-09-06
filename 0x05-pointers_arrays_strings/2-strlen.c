28e47319dc5d4cb942508a6e5d28634cc1260bea
NathyG2524 <Nathygirma2524@gmail.com>
2021-09-06 07:29:47 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen - is used to determine the length of a string.
+ * Return : not.
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+	return (c);
+}
