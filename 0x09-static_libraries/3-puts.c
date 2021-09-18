806a4fbbcf911892a219f7184cd5eddc6e85b27f
NathyG2524 <88330679+NathyG2524@users.noreply.github.com>
2021-09-18 22:27:03 +0300

    lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
- *_puts - prints a string, to stdout
- *@str: value to be evaluate
- *Retrun: not.
+ * _puts - Prints a string
+ * @str: The string to print
+ *
+ * Return: void
  */
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-	_putchar('\n');
-}
+for (; *str != '\0'; str++)
+{_putchar(*str); }
+_putchar('\n'); }
