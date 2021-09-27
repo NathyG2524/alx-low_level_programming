85c1aaed954b6eb03fb7f4d28d0d647bae35d962
NathyG2524 <Nathygirma2524@gmail.com>
2021-09-27 14:29:51 -0700

    dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,20 @@
 
 struct dog
 {
+        char *name;
+        float age;
+        char *owner;
+};
+#endif
+
+
+
+
+#ifndef DOG_H
+#define DOG_H
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
