9390903d4ce0b8826f9b9faeedb34466345bba1e
NathyG2524 <Nathygirma2524@gmail.com>
2021-10-12 04:33:45 -0700

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *free_listint2 - frees a listint_t
+ *head: the beginning of a list
+ *Returns: null
+ */
+
+void free_listint2(listint_t **head)
+{
+	*head = NULL;
+	free(*head);
+}
