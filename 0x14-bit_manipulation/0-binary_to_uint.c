36f9d29ce0c849f4b6dbab2a6dcf8d71ab6d757f
NathyG2524 <Nathygirma2524@gmail.com>
2021-10-15 01:25:54 -0700

    fix bug

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,13 +12,12 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int conv = 0;
 
 	if (!b)
-		return 0;
+		return (0);
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' && b[i] > '1')
-			return 0;
-		else
-			conv = 2 * conv + (b[i] - '0');
+			return (0);
+		conv = 2 * conv + (b[i] - '0');
 	}
-	return conv;
+	return (conv);
 }
