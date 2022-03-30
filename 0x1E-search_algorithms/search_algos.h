ff334509f052fbb01a8ef8a6cb8db1dcdfe4760c
root <nathygirma2524@gmail.com>
2022-03-30 12:32:08 -0700

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef _SEARCHALGO
+#define _SEARCHALGO
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
